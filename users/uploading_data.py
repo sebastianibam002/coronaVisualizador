bf01920ef3a9d6d9fe558e26b1780b60e605564b
sebastianibam002 <sebastianibam@gmail.com>
2021-05-12 23:32:34 -0500

    change on the download file

--- users/uploading_data.py
+++ users/uploading_data.py
@@ -67,7 +67,7 @@ def download_file():
         destination_writer = csv.writer(destination, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         all = Afiliado.objects.all()
         for element in all:
-            destination_writer.writerow([element.document_number, element.state])
+            destination_writer.writerow([element.document_number, element.first_val, element.second_val, element.third_val, element.state])
         
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
