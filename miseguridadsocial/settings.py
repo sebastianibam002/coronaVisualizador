ee50c8dbc89d976dc46a720ca9fc540987013b11
sebastianibam002 <55999978+sebastianibam002@users.noreply.github.com>
2021-06-24 14:11:02 -0500

    chnage the source file

--- miseguridadsocial/settings.py
+++ miseguridadsocial/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
+            'read_default_file': 'my.cnf',
         },
     }
 }
