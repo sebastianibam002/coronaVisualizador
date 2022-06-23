8a685ee32debdd4d05ae5de6f31b7fb9eb2383e8
sebastianibam002 <sebastianibam@gmail.com>
2022-06-22 20:50:27 -0400

    remove the secret key and the debug

--- miseguridadsocial/settings.py
+++ miseguridadsocial/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bp319cvl$qf&h$zsu#^iji+wkrapl7_5cp7o-o%had(q+^+oay'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['visualizadorseguridadsocial.herokuapp.com', '127.0.0.1']
 
@@ -94,6 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
